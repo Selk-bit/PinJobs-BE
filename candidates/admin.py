b3a626297708bbb66ed4d472d7983a6f52685415
unknown <selkellouti233@gmail.com>
2025-02-17 12:26:55 +0100

    tailored cv from career

--- candidates/admin.py
+++ candidates/admin.py
@@ -259,8 +259,8 @@ class LocationAdmin(admin.ModelAdmin):
 
 @admin.register(GeneralSetting)
 class GeneralSettingAdmin(admin.ModelAdmin):
-    list_display = ['ads_per_page', 'max_recent_search_terms', 'last_updated']
-    list_editable = ['ads_per_page', 'max_recent_search_terms']
+    list_display = ['ads_per_page', 'max_recent_search_terms', 'credits_to_start_with', 'num_of_careers_to_generate', 'last_updated']
+    list_editable = ['ads_per_page', 'max_recent_search_terms', 'credits_to_start_with', 'num_of_careers_to_generate']
     list_display_links = None
     help_texts = {"ads_per_page": "Set the number of ads displayed on each page."}
 
